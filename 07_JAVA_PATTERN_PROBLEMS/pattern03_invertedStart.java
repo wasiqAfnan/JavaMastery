2a57e5a313173852f590640cd18e6bebe8391db7
wasiqafnan <wansari964@yahoo.com>
2025-01-24 14:06:55 +0530

    Changes in pattern folder

--- 07_JAVA_PATTERN_PROBLEMS/pattern03_invertedStart.java
+++ 07_JAVA_PATTERN_PROBLEMS/pattern03_invertedStart.java
@@ -13,5 +13,15 @@ import java.util.Scanner;
 public class pattern03_invertedStart {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.print("How many rows: ");
+        int rows = sc.nextInt();
+
+        for(int i = rows; i > 0; i--){
+            for(int j = 0; j < i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
