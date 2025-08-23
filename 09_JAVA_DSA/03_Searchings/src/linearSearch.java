d7291258b1b64619fffc8d500cfd6656b11f7536
wasiqafnan <wansari964@yahoo.com>
2025-08-23 23:41:50 +0530

    Implement linear search func

--- 09_JAVA_DSA/03_Searchings/src/linearSearch.java
+++ 09_JAVA_DSA/03_Searchings/src/linearSearch.java
@@ -1,11 +1,27 @@
 public class linearSearch {
     public static void display(int[] nums) {
-        for(int i=0;i<nums.length; i++){
+        for (int i = 0; i < nums.length; i++) {
             System.out.print(nums[i] + " ");
         }
     }
 
+    // Linear Search Implementation
     public static void linearSearchfn(int[] nums) {
+        int key = 90; // the element we want to search
+        boolean found = false;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == key) {
+                System.out.println("Element " + key + " found at index " + i);
+                found = true;
+                break; // stop after finding
+            }
+        }
+
+        if (!found) {
+            System.out.println("Element " + key + " not found in the array.");
+        }
+
     }
 
     public static void main(String[] args) {
